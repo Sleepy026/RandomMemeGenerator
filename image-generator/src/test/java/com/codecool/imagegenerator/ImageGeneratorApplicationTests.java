1852e7ee40dce2a0f9c82f3e96050c3f7acb6d6d
David Korpos <korposdavid@gmail.com>
2019-12-16 14:03:46 +0100

    init image-generator module

---
+++ image-generator/src/test/java/com/codecool/imagegenerator/ImageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.imagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
