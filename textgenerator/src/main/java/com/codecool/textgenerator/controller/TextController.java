abd5700ce64166ce9a1291fcb7f1ca4dc6df8530
Albi012 <mayeralbert12@gmail.com>
2019-12-16 14:00:26 +0100

    One test route for text generating.

---
+++ textgenerator/src/main/java/com/codecool/textgenerator/controller/TextController.java
@@ -0,0 +1,23 @@
+package com.codecool.textgenerator.controller;
+
+import com.codecool.textgenerator.service.DataManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/text-generator")
+public class TextController {
+
+    @Autowired
+    private DataManager dataManager;
+
+    @GetMapping("/get-generated-text")
+    public List<String> getGeneratedText(){
+        return dataManager.getGeneratedText();
+    }
+
+}
