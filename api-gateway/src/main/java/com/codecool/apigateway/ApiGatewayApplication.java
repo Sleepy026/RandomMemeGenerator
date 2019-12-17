34e77b10ff5892ceb1f34c49852772f94db284a4
Tamas Floch <tamasfloch95@gmail.com>
2019-12-17 10:29:45 +0100

    Updated pom.xml with swagger2, removed spring cloud version and changed to RELEASE

--- api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
@@ -12,8 +12,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
