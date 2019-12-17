090be6736efd53806fe156e11404d14c157334cb
Sleepy026 <mrbrowngarden@gmail.com>
2019-12-17 11:31:13 +0100

    meme storage added to zuul

--- memestore/src/main/java/com/codecool/memestore/controller/MemeController.java
+++ memestore/src/main/java/com/codecool/memestore/controller/MemeController.java
@@ -1,8 +1,15 @@
 package com.codecool.memestore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MemeController {
 
+    @GetMapping
+    public String test(){
+        return "test";
+    }
 }
