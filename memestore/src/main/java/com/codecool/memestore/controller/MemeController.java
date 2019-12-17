055a2918fc2c8d04e570a317c544c8a5a2c5cd9f
Tamas Floch <tamasfloch95@gmail.com>
2019-12-17 15:05:41 +0100

    created up/down votes

--- memestore/src/main/java/com/codecool/memestore/controller/MemeController.java
+++ memestore/src/main/java/com/codecool/memestore/controller/MemeController.java
@@ -2,13 +2,16 @@ package com.codecool.memestore.controller;
 
 import com.codecool.memestore.model.Meme;
 import com.codecool.memestore.service.MemeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class MemeController {
@@ -16,13 +19,18 @@ public class MemeController {
     @Autowired
     private MemeService memeService;
 
-    @GetMapping
-    public String test() {
-        return "test";
-    }
-
     @GetMapping("/leader-board")
     public List<Meme> getLeaderBoard(){
         return memeService.getLeaderBoard();
+    }
+
+    @GetMapping("/up-vote/{memeId}")
+    public void upVote(@PathVariable("memeId") long memeId) {
+        memeService.upVote(memeId);
+    }
+
+    @GetMapping("/down-vote/{memeId}")
+    public void downVote(@PathVariable("memeId") long memeId) {
+        memeService.downVote(memeId);
     }
 }
--- memestore/src/main/java/com/codecool/memestore/service/MemeService.java
+++ memestore/src/main/java/com/codecool/memestore/service/MemeService.java
@@ -2,12 +2,14 @@ package com.codecool.memestore.service;
 
 import com.codecool.memestore.model.Meme;
 import com.codecool.memestore.repository.MemeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MemeService {
 
     @Autowired
@@ -21,5 +23,15 @@ public class MemeService {
         return memeRepository.findAllByOrderByUpVoteDesc();
     }
 
+    public void upVote(Long memeId) {
+        Meme meme = memeRepository.findById(memeId).get();
+        meme.setUpVote(meme.getUpVote() + 1);
+        save(meme);
+    }
 
+    public void downVote(Long memeId) {
+        Meme meme = memeRepository.findById(memeId).get();
+        meme.setDownVote(meme.getDownVote() + 1);
+        save(meme);
+    }
 }
