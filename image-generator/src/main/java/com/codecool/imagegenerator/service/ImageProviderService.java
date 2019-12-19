f0dfd3ca126e0c86d663fa09fdd52f917f5b7a95
David Korpos <korposdavid@gmail.com>
2019-12-19 14:38:45 +0100

    add more image templates

--- image-generator/src/main/java/com/codecool/imagegenerator/service/ImageProviderService.java
+++ image-generator/src/main/java/com/codecool/imagegenerator/service/ImageProviderService.java
@@ -14,12 +14,22 @@ public class ImageProviderService {
 
     private final static Map<String, String> templates = Map.ofEntries(
             entry("61579", "One-Does-Not-Simply"),
-            entry("100947", "What-If-I-Told-You"),
+            entry("87743020", "Two-Buttons"),
             entry("129242436", "Change-My-Mind"),
             entry("438680", "Batman-Slapping-Robin"),
             entry("114585149", "Inhaling-Seagull"),
             entry("27813981", "Hide-the-Pain-Harold"),
             entry("91545132", "Trump-Bill-Signing"),
+            entry("155067746", "Surprised-Pikachu"),
+            entry("119139145", "Blank-Nut-Button"),
+            entry("178591752", "Tuxedo-Winnie-The-Pooh"),
+            entry("188390779", "Woman-Yelling-At-Cat"),
+            entry("132769734", "Hard-To-Swallow-Pills"),
+            entry("123999232", "The-Scroll-Of-Truth"),
+            entry("184801100", "Me-And-The-Boys"),
+            entry("101287", "Third-World-Success-Kid"),
+            entry("99683372", "Sleeping-Shaq"),
+            entry("101910402", "Who Would Win?"),
             entry("718432", "Back-In-My-Day")
     );
 
